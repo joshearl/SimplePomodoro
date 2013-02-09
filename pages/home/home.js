e472e5552ce37b0bc19788e614acaf33f3c0b3a9
Josh Earl <joshearl928@gmail.com>
2013-02-09 07:11:15 -0500

    Initial commit
    Created project from template, added simple hello world on button click.

---
+++ pages/home/home.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        ready: function (element, options) {
+            /* TODO v1.0
+             * Start countdown from 25:00.
+             * Start countdown from 5:00.
+             * Start countdown from 15:00.
+             * Reset current countdown.
+             * Notification when timer is up.
+             * Analytics.
+             * Run in background.
+             * Settings to change background color.
+             * Settings to change length of time periods.
+
+            */
+            WinJS.Utilities.query("#start-button")
+                .listen("click", function () {
+                    var timeDisplay = WinJS.Utilities.query('#time-display');
+                    timeDisplay.forEach(function (div) { div.innerHTML = "hello world"; });
+                });
+        }
+    });
+})();
