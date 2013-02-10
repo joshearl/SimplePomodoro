1fba1b8085f69c02e73d7b4bb89891f40f0db0eb
Josh Earl <joshearl928@gmail.com>
2013-02-10 08:45:06 -0500

    Added break timer and stop feature.

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,6 +4,7 @@
     WinJS.UI.Pages.define("/pages/home/home.html", {
         ready: function (element, options) {
             /* TODO v1.0
+
              * Start countdown from 25:00.
              * Start countdown from 5:00.
              * Start countdown from 15:00.
@@ -20,25 +21,58 @@
                 fifteen: 900000,
                 twentyFive: 1500000
             };
-            
-            var countdown = {
-                start: new Date().getTime(),
-                remaining: function () {
-                    var now = new Date();
-                    var end = new Date(this.start + minutes.twentyFive);
 
-                    return ((end - now.getTime()) / 1000) / 60;
-                }
-            };
-            
-            var startButton = WinJS.Utilities.query('#start-button'),
+            var countdown,
+                ticker,
+                startWorkButton = WinJS.Utilities.query('#start-work'),
+                startBreakButton = WinJS.Utilities.query('#start-break'),
+                stopButton = WinJS.Utilities.query('#stop'),
                 timeDisplay = WinJS.Utilities.query('#time-display');
-                
-            startButton.listen("click", start);
+
+            startWorkButton.listen("click", function () { start(minutes.twentyFive); });
+            startBreakButton.listen("click", function () { start(minutes.five); });
+            stopButton.listen("click", reset);
             
-            function start() {
-                timeDisplay.forEach(function (div) { div.innerHTML = countdown.remaining(); });
-                window.setTimeout(start, 500);
+            function start(duration) {
+                countdown = getCountdown(duration);
+                update();
+            }
+
+            function update() {
+                var remaining = countdown.remaining();
+
+                if (remaining > 0) {
+                    setDisplayTo(remaining);
+                    scheduleNextUpdate();
+                } else {
+                    reset();
+                }
+            }
+
+            function setDisplayTo(value) {
+                timeDisplay.forEach(function (div) { div.innerHTML = value; });
+            }
+
+            function scheduleNextUpdate() {
+                ticker = window.setTimeout(update, 200);
+            }
+            
+            function reset() {
+                window.clearTimeout(ticker);
+                countdown = {};
+                setDisplayTo("00:00");
+            }
+
+            function getCountdown(length) {
+                return {
+                    start: new Date().getTime(),
+                    remaining: function () {
+                        var now = new Date(),
+                            end = new Date(this.start + length);
+
+                        return ((end - now.getTime()) / 1000) / 60;
+                    }
+                };
             }
         }
     });
