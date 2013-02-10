5c338d35dad0cde9ca4886ac176fbf504b53ce5e
Josh Earl <joshearl928@gmail.com>
2013-02-10 09:46:49 -0500

    Added toast notifications
    Combined pomodoro lengths and messages into an associative array. Added
    YeahToast library to handle displaying toast notifications. Implemented
    toast notifications on pomodoro completion.

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,8 @@
 ﻿(function () {
     "use strict";
 
+    var toast = YeahToast;
+
     WinJS.UI.Pages.define("/pages/home/home.html", {
         ready: function (element, options) {
             /* TODO v1.0
@@ -9,44 +11,47 @@
              * Stack buttons.
              * Hide duration buttons when clock is running.
              * Hide stop button when clock is stopped.
-             * Pop up toast notification when timer is up.
              * Track use with Google Analytics.
              * Run in background.
              * Settings to change background color.
              * Settings to change length of time periods.
 
             */
-            var minutes = {
-                five: 300000,
-                fifteen: 900000,
-                twentyFive: 1500000
-            };
 
+            var pomodoros = {
+                shortBreak: { length: 300000, completedMessage: "Break's over. Back to work!" },
+                longBreak: { length: 900000, completedMessage: "Break's over. Back to work!" },
+                work: { length: 1500000, completedMessage: "Good work! Let's take a break." }
+            };
+            
             var countdown,
                 ticker,
+                currentPomodoro,
                 workButton = WinJS.Utilities.query('#work'),
                 shortBreakButton = WinJS.Utilities.query('#short-break'),
                 longBreakButton = WinJS.Utilities.query('#long-break'),
                 stopButton = WinJS.Utilities.query('#stop'),
                 timeDisplay = WinJS.Utilities.query('#time-display');
 
-            workButton.listen("click", function () { start(minutes.twentyFive); });
-            shortBreakButton.listen("click", function () { start(minutes.five); });
-            longBreakButton.listen("click", function () { start(minutes.fifteen); });
+            workButton.listen("click", function () { start(pomodoros.work); });
+            shortBreakButton.listen("click", function () { start(pomodoros.shortBreak); });
+            longBreakButton.listen("click", function () { start(pomodoros.longBreak); });
             stopButton.listen("click", reset);
             
-            function start(duration) {
-                countdown = getCountdown(duration);
+            function start(pomodoro) {
+                currentPomodoro = pomodoro;
+                countdown = getCountdown(currentPomodoro.length);
                 update();
             }
 
             function update() {
                 var remaining = countdown.remaining();
-
+                
                 if (remaining > 0) {
                     setDisplayTo(remaining);
                     scheduleNextUpdate();
                 } else {
+                    showToast(currentPomodoro.completedMessage);
                     reset();
                 }
             }
@@ -68,6 +73,7 @@
             function getCountdown(length) {
                 return {
                     start: new Date().getTime(),
+                    type: "shortBreak",
                     remaining: function () {
                         var now = new Date(),
                             end = new Date(this.start + length);
@@ -75,6 +81,10 @@
                         return ((end - now.getTime()) / 1000) / 60;
                     }
                 };
+            }
+
+            function showToast(message) {
+                toast.show({ title: "SIMPLE POMODORO", textContent: message });
             }
         }
     });
